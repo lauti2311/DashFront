8ff8181bb91aedf8cc5eff32ba60756ac822e4e6
luki carrada <luki.carrada@gmail.com>
2024-07-11 15:48:32 -0300

    ArticuloInsumoShortDto

---
+++ src/types/dto/ArticuloInsumoShortDto.ts
@@ -0,0 +1,22 @@
+import DataModel from "../DataModel";
+import IUnidadMedida from "../UnidadMedida";
+import CategoriaShorDto from "./CategoriaDto";
+import SucursalDto from "./SucursalDto";
+
+
+interface ArticuloInsumoShortDto extends DataModel<ArticuloInsumoShortDto>{
+    id: number;
+    denominacion: string;
+    eliminado: boolean;
+    precioVenta: number;
+    unidadMedida: IUnidadMedida;
+    precioCompra: number;
+    stockActual: number;
+    stockMaximo: number;
+    stockMinimo: number;
+    esParaElaborar: boolean;
+    categoria: CategoriaShorDto;
+    sucursal: SucursalDto;
+}
+
+export default ArticuloInsumoShortDto;
